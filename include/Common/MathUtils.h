55206b47e88f648b90807e3b5d3ae3b69972ef29
Dustin Biser <dustinbiser@gmail.com>
2012-04-08 00:44:39 -0400

    Ran doxygen to update document files

--- include/Common/MathUtils.h
+++ include/Common/MathUtils.h
@@ -12,7 +12,13 @@
 namespace Rigid2D
 {
   int or2d(Vector2 & pt1, Vector2 & pt2, Vector2 & pt3);
+
   Real det3(Real a0, Real a1, Real a2, Real b0, Real b1, Real b2, Real c0, Real c1, Real c2);
+
+	/// Returns the centroid, represented as a Vector2, of the polygon
+	/// whos vertices are given by the parameter <vertices>.
+	Vector2 centroid (unsigned int vertice_count, Vector2 **vetices);
+
 }
 
 #endif
