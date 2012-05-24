0d478dbd07adad36b257c976a832234a9e5797d9
Dustin Biser <dustinbiser@gmail.com>
2012-05-24 01:27:27 -0400

    Updated method signatures withint NarrowPhase.h and added comment regarding msv_dir within Contact struct

--- include/Collision/NarrowPhase/NarrowPhase.h
+++ include/Collision/NarrowPhase/NarrowPhase.h
@@ -10,34 +10,37 @@ namespace Rigid2D {
   struct Contact {
       RigidBody *a;	            // Reference body.  The msv is computed with respect to this body.
       RigidBody *b;	            // Incident body that body a is colliding with.
-      Vector2 msv; 	            // Minimum separation vector, for translating a out of b.
+
+      Vector2 msv_dir;          // Direction of minimum separation vector (msv).
+                                // Needed by resolveCollision().  This direction
+                                // should be outward pointing from body B and
+                                // orthongal to B's contact edge.
 
       // Each Rigid Body contains a vertex list, and the following indices
       // allow us to keep track of specific vertices involved in the contact
-      // at various points in time.
-      unsigned int va_index;    // Vertex index for vertex of a that is contained in b.
-      unsigned int vb1_index; 	// Vertex index for first vertex that composes contact edge of b.
-      unsigned int vb2_index; 	// Vertex index for second vertex that composes contact edge of b.
+      // at various moments in time.
+      unsigned int va_index;     // Index of vertex of a that is contained in b.
+      unsigned int vb1_index;    // Index of first vertex that composes contact edge of b.
+      unsigned int vb2_index;    // Index for second vertex that composes contact edge of b.
 
-      // TODO: implement constructor, accessors, and mutators.
+      // TODO: implement constructor, and accessors
   };
 
   // Separating axis test
+  // Returns true if inter-penetration is detected between Rigid Bodys rb1 and
+  // rb2.  Otherwise, false is returned. If inter-penetration is detected the
+  // contact structure will be filled out, otherwise it will not be modified.
   bool sat(RigidBody *rb1, RigidBody *rb2, Contact &contact);
 
-  // Returns the time of impact (toi) between the two rigid bodies referenced
-  // by the given contact structure.  The referenced rigid bodies are assumed
-  // to be currently overlapping.  The returned toi value is the time just
-  // before now when the bodies were within some epsilon away from each other,
-  // and were considered "touching".
-  Real toi(Contact const &contact, Real current_system_time);
+  // Time of Impact
+  // Determines the contact time t_c of the two bodies referenced by the given
+  // Contact argument and rolls back their RBStates to the moment when they are
+  // non-overlapping and are within a distance epsilon > 0 from one another.
+  void toi(Contact const &contact);
 
-  // Shortest distance between the vertex va, and the edge that is composed of
-  // the vertices vb1 and vb2.
-  //
-  // TODO: change to "pointToLineDist(...)" and move to folder include/Math?
-  Real minDistance(Contact const &contact);
-
+  // Adjusts the linear velocities of the two Rigid Bodys referenced by the
+  // given Contact arugment in order to simulate an elastic collision.
+  void resolveCollision(Contact const &contact);
 }
 
 #endif
