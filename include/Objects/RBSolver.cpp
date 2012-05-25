0b292d5a20b9ea896661b417d0c8d98c1b90cece
Dustin Biser <dustinbiser@gmail.com>
2012-05-25 03:18:25 -0400

    Added const static member step_size to RBSolver in order to retreive default step size.  Needed for toi calculation.

--- include/Objects/RBSolver.cpp
+++ include/Objects/RBSolver.cpp
@@ -2,6 +2,8 @@
 
 using namespace Rigid2D;
 
+const Real RBSolver::step_size = 0.01;
+
 void RBSolver::nextStep(class RigidBody &rb, struct RBState &outState, Real stepSize) {
   struct RBState initialState, tempState, k1, k2, k3, k4;
 
